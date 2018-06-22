5f7b9bb3efe8a7aad8bcb06582453f6833a7cb41
maddhruv <dhruvjainpenny@gmail.com>
2018-06-22 11:47:52 +0530

    webpack and express setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+
+const watchOptions = {
+  ignored: /(node_modules)/
+}
+
+const browserConfig = {
+  entry: './src/browser/',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  watchOptions
+}
+
+const serverConfig = {
+  entry: './src/server/index.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'server'),
+    filename: 'main.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: { presets: ['react-app'] }
+      }
+    ]
+  },
+  watchOptions
+}
+
+module.exports = [browserConfig, serverConfig]
